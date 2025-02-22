aaade205375467a6f62b84d42ed2fd6c280ce3f9
diegoyl <diegoyanez100@gmail.com>
2025-02-22 12:38:57 -0600

    u

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ import {AnimatePresence} from 'framer-motion';
 import TestingPage from './TestingPage';
 
 import {useLocation} from "react-router-dom"
-import {  HashRouter as Router, Routes, Route } from "react-router-dom";
+import {  BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
-//   <Link to={to} onClick={delayAndGo}>
 
 
 function App({page}) {
