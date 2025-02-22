c03aa0ab57c88daf34ee69e90b879805f0663c41
diegoyl <diegoyanez100@gmail.com>
2025-02-22 00:04:32 -0600

    u

--- src/index.js
+++ src/index.js
@@ -7,40 +7,39 @@ import ProjectPage from './ProjectPage';
 import About from './About';
 import TestingPage from './TestingPage';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 // import { AnimatePresence } from 'framer-motion';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <AnimatePresence> */}
+
       <Router>
         <Routes>
           <Route path="/"
               element={<HomePage title="Diego Yañez-Laguna"/>}
-              errorElement={<div>404 Not Found</div>}
+              errorElement={<div>oops! 404 Not Found</div>}
           />
           <Route path="/projects"
               element={<Projects title="Projects"/>}
-              errorElement={<div>404 Not Found</div>}
+              errorElement={<div>oops! 404 Not Found</div>}
           />
           <Route path="/:projectName"
               element={<ProjectPage />}
-              errorElement={<div>404 Not Found</div>}
+              errorElement={<div>oops! 404 Not Found</div>}
           />
           <Route path="/about"
               element={<About title="About"/>}
-              errorElement={<div>404 Not Found</div>}
+              errorElement={<div>oops! 404 Not Found</div>}
           />
           <Route path="/testing"
               element={<TestingPage title="TP"/>}
-              errorElement={<div>404 Not Found</div>}
+              errorElement={<div>oops! 404 Not Found</div>}
           />
         </Routes>
       </Router>
-    {/* </AnimatePresence> */}
-  </React.StrictMode>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
